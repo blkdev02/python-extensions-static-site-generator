a77b3eb85d77cca2a5387705e5ea971a872cd436
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-23 03:52:53 +0100

    4.9-10 checking

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,7 +6,8 @@ from pathlib import Path
 
 from docutils.core import publish_parts
 from markdown import markdown
-from ssg.content import Content, hooks
+from ssg.content import Content
+from ssg import hooks
 
 
 class Parser:
