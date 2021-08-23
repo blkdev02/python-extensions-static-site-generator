5d85d1a79a6a420bacc5452951996709130d2e71
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-23 04:18:50 +0100

    5.1-3 checking

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -4,3 +4,12 @@ import time
 start_time = None
 total_written = 0
 
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.time()
+
+@hooks.register("written")
+def written():
+    global total_written
+    total_written += 1
