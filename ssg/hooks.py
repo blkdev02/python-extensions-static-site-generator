a7006cc936769cf8ae4aa2f26d72881acfd10a47
David Boadu <david.boadu@sainsburys.co.uk>
2021-08-23 01:02:26 +0100

    3.3 checking

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -3,8 +3,7 @@ _callbacks = {}
 def register(hook, order=0):
 
     def register_callback(func):
-        _callbacks.setdefault(hook,{})
-        _callbacks.setdefault(order, {})
+        _callbacks.setdefault(hook,{}, order, {})
         _callbacks.append(func)
         return func
     return register_callback
